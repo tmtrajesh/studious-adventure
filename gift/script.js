34ceb80bf683c03258672e7a6077d45ab0e5d434
unknown <taniyarajeshmt@gmail.com>
2025-02-02 15:49:11 +0530

    first ver fix

--- gift/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('surpriseBtn').addEventListener('click', () => {
-    const message = document.getElementById('message');
-    message.innerHTML = "I Love You So Much! 💕";
-    message.style.transition = "transform 1s ease-in-out";
-    message.style.transform = "scale(1.5)";
-});
